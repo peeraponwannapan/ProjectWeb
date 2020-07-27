964a5d2bb36d023c750424098d3091e314d0d490
peeraponwannapan <manmeisterz@gmail.com>
2020-07-28 00:56:20 +0700

    README

---
+++ start.js
@@ -0,0 +1,42 @@
+(()=>{
+
+
+  function getCurrentInvetory(){
+      var spreadsheetID = "1YbejU_b2OpqyF2htEgZxAPrrb9uNwINduryeJdXcEDM";
+      var sheetTab = "3";
+      var url = "https://spreadsheets.google.com/feeds/list/" + spreadsheetID + "/"+sheetTab+"/public/values?alt=json";
+      console.log(url);
+      $.getJSON(url, function(data) {
+          var entry = data.feed.entry;
+          // var length = entry.length;
+          $(".Student-acc").remove();
+
+          $(entry).each(function(index, element){
+
+              // if (index === (length - 1)) {
+              //     $('#total').text(this.gsx$value.$t);
+              // }else{
+                  var inventoryItem = $('<tr class="Student-acc">').appendTo('.table');
+                  inventoryItem.append(
+                      $(document.createElement('td')).text(this.gsx$time.$t).addClass('acc-StudentID')
+                  );
+                  inventoryItem.append(
+                      $(document.createElement('td')).text(this.gsx$studentid.$t).addClass('acc-Firstname')
+                  );
+                  inventoryItem.append(
+                      $(document.createElement('td')).text(this.gsx$firstname.$t).addClass('acc-Lastname')
+                  );
+                  inventoryItem.append(
+                      $(document.createElement('td')).text(this.gsx$lastname.$t).addClass('acc-TimeStamp')
+                  );
+                         // }
+                  });
+      });
+      setTimeout(getCurrentInvetory, 300);
+  } 
+  function run(){
+    getCurrentInvetory();
+  }  
+  run();
+})();
+
